a2ae28c642924139b4854d614dbc5d74342cb711
iil <ioann.Labudev@yandex.ru>
2020-10-15 13:05:51 +0300

    Сделал нормальный переключатель между режимами и сделал тост по окончанию списка.

--- app/src/main/java/com/ivan/mymovies/MainActivity.java
+++ app/src/main/java/com/ivan/mymovies/MainActivity.java
@@ -6,6 +6,10 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.ivan.mymovies.data.Movie;
@@ -18,19 +22,68 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Switch switshSort;
     private RecyclerView recyclerViewPoster;
     private MovieAdapter movieAdapter;
+    private TextView textViewTopRated;
+    private TextView textViewPopularity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        switshSort = findViewById(R.id.switchSort);
+        textViewPopularity = findViewById(R.id.textViewPopularity);
+        textViewTopRated = findViewById(R.id.textViewTopRated);
         recyclerViewPoster = findViewById(R.id.recyclerViewPosters);
         recyclerViewPoster.setLayoutManager(new GridLayoutManager(this,2));
         movieAdapter = new MovieAdapter();
-        JSONObject jsonObject = NetworkUtils.getJSONFromNetwork(NetworkUtils.POPULARITY,1);
+        recyclerViewPoster.setAdapter(movieAdapter);
+        switshSort.setChecked(true);
+        switshSort.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                setMethodOfSort(isChecked);
+            }
+        });
+        switshSort.setChecked(false);
+        movieAdapter.setOnPosterClickListener(new MovieAdapter.OnPosterClickListener() {
+            @Override
+            public void onPosterClick(int position) {
+                Toast.makeText(MainActivity.this,"Clicked: " + position, Toast.LENGTH_SHORT).show();
+            }
+        });
+        movieAdapter.setOnReachEndListener(new MovieAdapter.OnReachEndListener() {
+            @Override
+            public void onReachEnd() {
+                Toast.makeText(MainActivity.this, "Конец списка", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public void onClickSetPopularity(View view) {
+        setMethodOfSort(false);
+        switshSort.setChecked(false);
+    }
+
+    public void onClickSetTopRated(View view) {
+        setMethodOfSort(true);
+        switshSort.setChecked(true);
+    }
+
+    private void setMethodOfSort(boolean isTopRated) {
+        int methodOfSort;
+        if  (isTopRated) {
+            textViewTopRated.setTextColor(getResources().getColor(R.color.colorAccent));
+            textViewPopularity.setTextColor(getResources().getColor(R.color.white_color));
+            methodOfSort = NetworkUtils.TOP_RATED;
+        } else {
+            textViewPopularity.setTextColor(getResources().getColor(R.color.colorAccent));
+            textViewTopRated.setTextColor(getResources().getColor(R.color.white_color));
+            methodOfSort = NetworkUtils.POPULARITY;
+        }
+        JSONObject jsonObject = NetworkUtils.getJSONFromNetwork(methodOfSort,1);
         ArrayList<Movie> movies = JSONUtils.getMoviesFromJSON(jsonObject);
         movieAdapter.setMovies(movies);
-        recyclerViewPoster.setAdapter(movieAdapter);
     }
 }
