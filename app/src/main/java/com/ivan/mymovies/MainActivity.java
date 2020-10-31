20c8c725dfb791f62cf854496c980f21b358f849
iil <ioann.Labudev@yandex.ru>
2020-10-31 12:57:17 +0300

    Добавил базу данных, для хранения изображений, надо доработать

--- app/src/main/java/com/ivan/mymovies/MainActivity.java
+++ app/src/main/java/com/ivan/mymovies/MainActivity.java
@@ -1,6 +1,9 @@
 package com.ivan.mymovies;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -12,6 +15,7 @@ import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.ivan.mymovies.data.MainViewModel;
 import com.ivan.mymovies.data.Movie;
 import com.ivan.mymovies.utils.JSONUtils;
 import com.ivan.mymovies.utils.NetworkUtils;
@@ -19,6 +23,7 @@ import com.ivan.mymovies.utils.NetworkUtils;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,10 +33,13 @@ public class MainActivity extends AppCompatActivity {
     private TextView textViewTopRated;
     private TextView textViewPopularity;
 
+    private MainViewModel viewModel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        viewModel = ViewModelProviders.of(this).get(MainViewModel.class);
         switshSort = findViewById(R.id.switchSort);
         textViewPopularity = findViewById(R.id.textViewPopularity);
         textViewTopRated = findViewById(R.id.textViewTopRated);
@@ -59,6 +67,13 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "Конец списка", Toast.LENGTH_SHORT).show();
             }
         });
+        LiveData<List<Movie>> moviesFromLiveData = viewModel.getMovies();
+        moviesFromLiveData.observe(this, new Observer<List<Movie>>() {
+            @Override
+            public void onChanged(List<Movie> movies) {
+                movieAdapter.setMovies(movies);
+            }
+        });
     }
 
     public void onClickSetPopularity(View view) {
@@ -82,8 +97,18 @@ public class MainActivity extends AppCompatActivity {
             textViewTopRated.setTextColor(getResources().getColor(R.color.white_color));
             methodOfSort = NetworkUtils.POPULARITY;
         }
+        downloadData(methodOfSort,1);
+    }
+
+    private void downloadData(int methodOfSort, int page) {
         JSONObject jsonObject = NetworkUtils.getJSONFromNetwork(methodOfSort,1);
         ArrayList<Movie> movies = JSONUtils.getMoviesFromJSON(jsonObject);
+        if (movies != null && !movies.isEmpty()) {
+            viewModel.deleteAllMovie();
+            for (Movie movie : movies) {
+                viewModel.insertMovies(movie);
+            }
+        }
         movieAdapter.setMovies(movies);
     }
 }
